a59737bb6204958a476db68678dbb5ecb4e80038
ze6r4 <elenacerenkova603@gmail.com>
2025-12-10 12:28:18 +0400

    timer settings html

---
+++ src/main/resources/static/assets/front/js/timer-settings.js
@@ -0,0 +1,16 @@
+// const range = document.querySelector('.range-input')
+// range.addEventListener('input', handleInputRange)
+// function handleInputRange() {
+//   event.target.parentNode.parentNode.style.setProperty(
+//     '--value',
+//     event.target.value
+//   )
+// }
+// function handleInputRange() {
+//   event.target.parentNode.parentNode.style.setProperty(
+//     '--value',
+//     event.target.value
+//   )
+//   // изменение значения тега `<output>`
+//   event.target.nextElementSibling.value = event.target.value;
+// }
